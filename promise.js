8e25cc1f464939e0056288cee9f035b8c539cfc8
Alfian <alfian.org@gmail.com>
2022-06-12 20:15:19 +0700

    11 - Challenge Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (param) => {
+  try {
+    const [theaterIXX, theaterVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()]);
+    const result = [...theaterIXX, ...theaterVGC];
+    return result?.filter((item) => item.hasil === param)?.length || 0;
+  } catch (err) {
+    throw err;
+  }
+};
 
 module.exports = {
   promiseOutput,
